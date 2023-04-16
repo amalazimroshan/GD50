035e7240b34ee314403562dcf1a0666dbb208710
Amal Roshan <amalazimroshan@gmail.com>
2023-04-16 13:13:47 +0530

    added pipes

--- flappy bird/main.lua
+++ flappy bird/main.lua
@@ -9,6 +9,8 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+math.randomseed(os.time())
+
 local background = love.graphics.newImage('background.png')
 local backgroundScroll = 0
 local ground = love.graphics.newImage('ground.png')
@@ -18,8 +20,11 @@ local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
+
 local bird = Bird()
 
+local pipes = {}
+local spawnTimer = 0 
 function love.load()
     love.graphics.setDefaultFilter('nearest','nearest')
     love.window.setTitle('flappy bird')
@@ -52,12 +57,31 @@ function love.update(dt)
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
+    spawnTimer = spawnTimer + dt;
+    if spawnTimer > 2 then
+        table.insert(pipes,Pipe())
+        spawnTimer = 0
+    end
+
     bird:update(dt)
+
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+        if pipe.x < -pipe.width then
+            table.remove(pipes,k)
+        end
+    end
+
     love.keyboard.keysPressed = {}
 end
 function love.draw()
     push:start()
     love.graphics.draw(background,-backgroundScroll,0)
+    
+    for k,pipe in pairs(pipes) do
+        pipe:render()
+    end
+    
     love.graphics.draw(ground,-groundScroll,VIRTUAL_HEIGHT - 16)
     bird:render()
     push:finish()
