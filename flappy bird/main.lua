310e1c92784d66c60a51fd29d99d6ae2e6c4546d
Amal Roshan <amalazimroshan@gmail.com>
2023-05-02 22:17:00 +0530

    flappy bird revist done gravity

--- flappy bird/main.lua
+++ flappy bird/main.lua
@@ -1,15 +1,12 @@
 push = require 'push'
 Class = require 'class'
 require 'Bird'
-require "Pipe"
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
-
-math.randomseed(os.time())
 
 local background = love.graphics.newImage('background.png')
 local backgroundScroll = 0
@@ -18,70 +15,37 @@ local groundScroll = 0
 
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
-
 local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
 
-local pipes = {}
-local spawnTimer = 0 
 function love.load()
     love.graphics.setDefaultFilter('nearest','nearest')
-    love.window.setTitle('flappy bird')
+    love.window.setTitle('Flappy bird')
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
         vsync = true,
         fullscreen = false,
         resizable = true
     })
-    love.keyboard.keysPressed = {}
 end
-
 function love.resize(w,h)
     push:resize(w,h)
 end
-function love.keypressed(key)
-    love.keyboard.keysPressed[key] = true
-
+function love.keypressed(key)   
     if key == 'escape' then
         love.event.quit()
     end
 end
-function love.keyboard.wasPressed(key)
-    if love.keyboard.keysPressed[key] then
-        return true
-    else
-        return false
-    end
-end
 function love.update(dt)
-    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+     % BACKGROUND_LOOPING_POINT
+     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
-    spawnTimer = spawnTimer + dt;
-    if spawnTimer > 2 then
-        table.insert(pipes,Pipe())
-        spawnTimer = 0
-    end
-
-    bird:update(dt)
-
-    for k, pipe in pairs(pipes) do
-        pipe:update(dt)
-        if pipe.x < -pipe.width then
-            table.remove(pipes,k)
-        end
-    end
-
-    love.keyboard.keysPressed = {}
+     bird:update(dt)
 end
 function love.draw()
     push:start()
     love.graphics.draw(background,-backgroundScroll,0)
-    
-    for k,pipe in pairs(pipes) do
-        pipe:render()
-    end
-    
     love.graphics.draw(ground,-groundScroll,VIRTUAL_HEIGHT - 16)
     bird:render()
     push:finish()
